e5ec2b7f717d8712e05d3fc9bff82cd16c4060fc
venkatesan <venkatraman1729@gmail.com>
2020-05-09 21:10:27 +0530

    TECHMAN:CLOUD: Added init checkin for cloud learning

--- spring-cloud/spring-cloud-client/src/main/java/com/techmania/cloud/CloudClient.java
+++ spring-cloud/spring-cloud-client/src/main/java/com/techmania/cloud/CloudClient.java
@@ -1,9 +1,10 @@
 package com.techmania.cloud;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,17 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class CloudClient {
-    @Autowired
-    private Environment environment;
+    @Value("${user.role}")
+    private String role;
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudClient.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(CloudClient.class, args);
+        applicationContext.getEnvironment().getPropertySources()
+                .stream()
+                .distinct()
+                .map(PropertySource::getSource)
+                .forEach(System.out::println);
     }
 
     @GetMapping(
             value = "/whoami/{username}",
             produces = MediaType.TEXT_PLAIN_VALUE)
     public String whoami(@PathVariable("username") String username) {
-        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, environment.getProperty("user.role"));
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
     }
 }
--- spring-cloud/spring-cloud-server/src/main/java/com/techmania/cloud/CloudServer.java
+++ spring-cloud/spring-cloud-server/src/main/java/com/techmania/cloud/CloudServer.java
@@ -3,11 +3,12 @@ package com.techmania.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableConfigServer
 public class CloudServer {
     public static void main(String[] args) {
-        SpringApplication.run(CloudServer.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(CloudServer.class, args);
     }
 }
